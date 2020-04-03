6f243a84b560c42c1d0994cb506a20f541c150ef
Matheus Castello <matheus@castello.eng.br>
2020-04-03 13:26:40 -0300

    gps: dotnet: console: Add console sample
    This example shows how to use a .net core console application to connect
    the board UART to a GPS and print longitude and latitude.
    
    Related-to: TOR-112
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ gps/dotnet/console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using NmeaParser;
+using NmeaParser.Messages;
+using System.IO.Ports;
+
+namespace gps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialDev;
+            SerialPortDevice gps;
+
+            // serial configs
+            serialDev = new SerialPort();
+            serialDev.PortName = Environment.GetEnvironmentVariable("GPS_SERIAL_PORT");
+            serialDev.BaudRate = 9600;
+            gps = new SerialPortDevice(serialDev);
+
+            // set listener
+            gps.MessageReceived += OnNmeaMessageReceived;
+            gps.OpenAsync();
+
+            // on ctrl+c close connection
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
+            {
+                gps.CloseAsync();
+            };
+
+            // wait for gps inputs
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void OnNmeaMessageReceived(object sender, NmeaMessageReceivedEventArgs args)
+        {
+            if (args.Message is Rmc rmc)
+            {
+                Console.WriteLine($"Latitude::{rmc.Latitude}\tLongitude::{rmc.Longitude}");
+            }
+        }
+    }
+}
