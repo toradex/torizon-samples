d213052211174be49edfe7396668500087124b45
Otavio Pontes <otavio.pontes@toradex.com>
2023-09-06 15:58:59 -0300

    pwm: C: Get PWM values from cli arguments, add comments and other minor improvements
    Signed-off-by: Otavio Pontes <otavio.pontes@toradex.com>

--- pwm/C/pwmC/src/pwm_utils.c
+++ pwm/C/pwmC/src/pwm_utils.c
@@ -10,92 +10,102 @@
 
 #define MAX_BUF 50
 
-int pwm_write_val(char *file, uint32_t val)
-{
-    int fd;
-    char buf[MAX_BUF]; 
-    
-    fd = open(file, O_WRONLY);
-    if(fd < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");                  
-        return -1;
-    }        
-    if( snprintf(buf, MAX_BUF, "%u", val) < 0)
-        goto failed; 
+// Write values to PWM files
+int pwm_write_val(char *pwm, char *file, uint64_t val) {
+	int fd;
+	char buf[MAX_BUF];
+	char path[MAX_BUF];
 
-    if( write(fd, buf, strlen(buf)) < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");                  
-        goto failed;
-    }
+	strcpy(path, pwm);
+	strcat(path, file);
+	fd = open(path, O_WRONLY);
 
-    close(fd);    
-    return 0;
+	if (fd < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		return -1;
+	}
+
+	if (snprintf(buf, MAX_BUF, "%u", val) < 0)
+		goto failed;
+
+	if (write(fd, buf, strlen(buf)) < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		goto failed;
+	}
+
+	close(fd);
+	return 0;
 
 failed:
-    close(fd);
-    return -1;
+	close(fd);
+	return -1;
 }
 
-int pwm_write_str(char *file, char *val)
-{
-    int fd;
-    
-    fd = open(file, O_WRONLY);
-    if(fd < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");
-        return -1;
-    }        
-    if( write(fd, val, strlen(val)) < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");                  
-        close(fd);
-        return -1;
-    }
+// Write strings to PWM files
+int pwm_write_str(char *pwm, char *file, char *val) {
+	int fd;
+	char path[MAX_BUF];
 
-    close(fd); 
-    return 0;
+	strcpy(path, pwm);
+	strcat(path, file);
+	fd = open(path, O_WRONLY);
+
+	if (fd < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		return -1;
+	}
+
+	if (write(fd, val, strlen(val)) < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
+	return 0;
 }
 
-int pwm_read(char *file, char *val, uint8_t size)
-{
-    int fd;
-    
-    fd = open(file, O_RDONLY);
-    if(fd < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");                          
-        return -1;
-    }
-    if( read(fd, val, size) < 0)
-    {
-        printf("Error Number % d\n", errno);  
-        perror("pwm");                          
-        close(fd);
-        return -1;    
-    }
- 
-    close(fd);     
-    return 0;
+// Read PWM files
+int pwm_read(char *pwm, char *file, char *val, uint8_t size) {
+	int fd;
+	char path[MAX_BUF];
+
+	strcpy(path, pwm);
+	strcat(path, file);
+	fd = open(path, O_RDONLY);
+
+	if (fd < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		return -1;
+	}
+
+	if ( read(fd, val, size) < 0) {
+		printf("Error Number % d\n", errno);
+		perror("pwm");
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
+	return 0;
 }
 
-int pwm_exists(char *file) {
-    
-    DIR* fd = opendir(file);
-    int exists;
-    if(fd)
-    {     
-        exists = 0;                      
-    } else {
-        exists = -1;
-    }
-    closedir(fd);  
-    return exists;
+// Check if there is a PWM channel
+int pwm_exists(char *path) {
+	DIR* fd = opendir(path);
+	int exists;
+
+	if(fd){
+		exists = 0;
+	} else {
+		exists = -1;
+	}
+
+	closedir(fd);
+	return exists;
 }
