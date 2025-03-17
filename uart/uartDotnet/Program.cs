7862f681530a523e53d2251fd7e1733464f6c856
Guilherme L Sousa <guilherme.sousa@toradex.com>
2025-03-17 16:13:39 -0300

    UART GPS DotNet

--- uart/uartDotnet/Program.cs
+++ uart/uartDotnet/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Torizon!");
+﻿using System;
+using System.Threading;
+using NmeaParser;           // Provides NmeaMessage, NmeaMessage.Parse(), etc.
+using NmeaParser.Messages;  // Provides Rmc, Gga, etc.
+using System.IO.Ports;
+
+namespace gps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialDev;
+            SerialPortDevice gps;
+
+            // Configure the serial port
+            serialDev = new SerialPort();
+            serialDev.PortName = Environment.GetEnvironmentVariable("GPS_SERIAL_PORT");
+            serialDev.BaudRate = 9600;
+
+            // Initialize the GPS device using the library’s SerialPortDevice
+            gps = new SerialPortDevice(serialDev);
+
+            // Subscribe to the MessageReceived event
+            gps.MessageReceived += OnNmeaMessageReceived;
+
+            // Open the connection asynchronously
+            gps.OpenAsync();
+
+            // On Ctrl+C, close the connection
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                gps.CloseAsync();
+            };
+
+            // Keep the application running indefinitely
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void OnNmeaMessageReceived(object sender, NmeaMessageReceivedEventArgs args)
+        {
+            // When an RMC message is received, print latitude and longitude
+            if (args.Message is Rmc rmc)
+            {
+                Console.WriteLine($"Latitude: {rmc.Latitude}\tLongitude: {rmc.Longitude}");
+            }
+        }
+    }
+}
