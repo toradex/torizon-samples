5dbc8cd0b4208d7e17f60de9668e8ba4068a4f2a
William Huster <whusterj@gmail.com>
2025-07-22 13:17:35 -0400

    Add support for binning (mode 2) of imx676 driver.

--- isp/imx8mp/framos/Torizon7/isp-imx/patches/patch.sh
+++ isp/imx8mp/framos/Torizon7/isp-imx/patches/patch.sh
@@ -3,3 +3,4 @@
 # Apply patches
 patch -d /downloads/isp-imx-${ISP_IMX_VERSION}/ -p1 < 0001-isp-imx-start_isp-don-t-report-error-if-no-camera-is.patch 
 patch -d /downloads/isp-imx-${ISP_IMX_VERSION}/ -p1 < 0002-Add-support-for-imx6xx-sensors.patch 
+patch -d /downloads/isp-imx-${ISP_IMX_VERSION}/ -p1 < 0003-add-support-for-imx676-mode-2-binning.patch
